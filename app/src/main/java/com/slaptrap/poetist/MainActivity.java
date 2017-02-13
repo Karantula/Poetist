76d561bc8f56288d11d1fbc8838ba05008933b1e
Ivan <karanovic.ivan@gmail.com>
2017-02-13 13:52:42 -0500

    First commit - unparsed data downloaded and displayed

---
+++ app/src/main/java/com/slaptrap/poetist/MainActivity.java
@@ -0,0 +1,85 @@
+package com.slaptrap.poetist;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    //ArrayList<String> poems = new ArrayList<>();
+
+
+    public void poetry(View view) {
+
+        //TextView poetryResult = (TextView) findViewById(R.id.poetryResult);
+        EditText keywordA = (EditText) findViewById(R.id.keywordA);
+
+
+        DownloadTask task = new DownloadTask();
+        task.execute("Paste the api url here" + keywordA.getText().toString());
+
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                int data = reader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data=reader.read();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            TextView poetryResult = (TextView)findViewById(R.id.poetryResult);
+            poetryResult.setText(result);
+            Log.i("Pjesme", result);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
