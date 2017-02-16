ec170ea620cd95e3a2fcb778d2a2e53c9023d65a
Ivan <karanovic.ivan@gmail.com>
2017-02-16 12:04:30 -0500

    First commit

--- app/src/main/java/com/slaptrap/poetist/MainActivity.java
+++ app/src/main/java/com/slaptrap/poetist/MainActivity.java
@@ -8,19 +8,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
